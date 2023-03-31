7291a7a6146a1e7797eb86dbc673189485146e2d
Anupamajha2 <anupamajha11233@gmail.com>
2023-03-31 18:53:03 +0530

    feat: access key page

--- onboarding-client/src/pages/_app.js
+++ onboarding-client/src/pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+
+
+
+ function App({Component,pageProps}){
+  return <Component {...pageProps} /> 
+   
+ }
+ export default App;
---
+++ onboarding-client/src/pages/navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return ( 
+        <header className="navbar">
+        <p>Barclays Cloud Customer Ingestion</p>
+        </header>
+     );
+}
+ 
+export default Navbar;
